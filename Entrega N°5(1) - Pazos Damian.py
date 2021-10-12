1ffa238813628c7a9d56345bddcd244bdea34100
Damian Pazos <damian_ariel_pazos>
2021-10-12 01:16:05 -0300

    Terminado

--- Entrega N°5(1) - Pazos Damian.py
+++ Entrega N°5(1) - Pazos Damian.py
@@ -4,9 +4,11 @@ que devuelva el área del rectángulo a partir de una base y una altura.
 Calcula el área de un rectángulo de 15 de base y 10 de altura
 '''
 
+# Inicio la funcion
 def area_rectangulo(base,altura):
-    return "El area de un rectangulo es {} metros cuadrados".format(base*altura)
+    return "El area de un rectangulo es {} metros cuadrados".format(base*altura) # Retorno el dato de la base
 
+# Inicio funcion para convertir los datos a numeros
 def convertir(dato):
 	while dato.isnumeric() == False:
 		print("¡Lo ingresado no es un número!")
@@ -14,10 +16,13 @@ def convertir(dato):
 	dato = int(dato)
 	return dato
 
+# Ingreso el numero el cual va ir en la funcion
 base_ingresada = input("Ingrese la base: \n")
-base_ingresada = convertir(base_ingresada)
+base_ingresada = convertir(base_ingresada) # La convierto en int
 
+# Ingreso el numero el cual va ir en la funcion
 altura_ingresada = input("Ingrese la altura: \n")
-altura_ingresada = convertir(altura_ingresada)
+altura_ingresada = convertir(altura_ingresada) # La convierto en int
 
-print(area_rectangulo(base_ingresada,altura_ingresada))
+print(area_rectangulo(base_ingresada,altura_ingresada)) # Aplico la funcion con los datos ingresados
+print(area_rectangulo(15,10)) # Aplico la funcion con los datos pedidos en el ejercicio
