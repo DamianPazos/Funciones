2daeeba5bcc0d1358d278d9143a4bf58d886ad3c
Damian Pazos <damian_ariel_pazos>
2021-10-12 01:20:46 -0300

    En desarrollo

---
+++ Entrega N°5(2) - Pazos Damian.py
@@ -0,0 +1,14 @@
+'''
+Realiza una función llamada area_circulo() que devuelva el área de un círculo a partir de un radio. 
+Calcula el área de un círculo de 5 de radio
+'''
+
+# Inicio la funcion pedida
+def area_circulo(radio):
+    constante_pi = 3.14159
+    area_total = constante_pi * radio * radio
+    return area_total
+
+print(area_circulo(5))
+
+
