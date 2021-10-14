e84f45cdc957e9e0570afb3bde8aa5cd2fcb263b
Damian Pazos <damian_ariel_pazos>
2021-10-14 19:55:33 -0300

    Terminado

--- Entrega N°5(2) - Pazos Damian.py
+++ Entrega N°5(2) - Pazos Damian.py
@@ -9,4 +9,17 @@ def area_circulo(radio):
     area_total = constante_pi * radio * radio # Calculo el area total
     return area_total # Retorno el area total del circulo
 
-print("El area total del circulo es", area_circulo(5)) # Imprimo el valor pedido
+# Inicio funcion para convertir los datos a numeros
+def convertir(dato):
+	while dato.isnumeric() == False: # Mientras que el dato del numero no sea un numero
+		print("¡Lo ingresado no es un número!")
+		dato = input("Ingrese el dato nuevamente: ") # Se pide nuevamente el dato
+	dato = int(dato) # Se convierte el dato 
+	return dato
+
+# Se ingresa el radio
+radio_ingresado = input("Ingrese el radio: \n")
+radio_ingresado = convertir(radio_ingresado)
+
+# Se muestra el resultado del area
+print("El area total del circulo es", area_circulo(radio_ingresado)) # Imprimo el valor pedido
