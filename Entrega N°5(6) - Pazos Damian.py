530b724b04b66fdb08745c4d3e980c14c9e79372
Damian Pazos <damian_ariel_pazos>
2021-10-16 14:37:52 -0300

    En desarrollo

---
+++ Entrega N°5(6) - Pazos Damian.py
@@ -0,0 +1,46 @@
+'''
+Realiza una función separar() que tome una lista de números enteros y devuelva dos listas ordenadas. 
+La primera con los números pares, 
+y la segunda con los números impares
+
+'''
+lista_ingresada = []
+
+def separar(lista_numeros):
+    lista_pares = []
+    lista_impares = []
+    for i in lista_numeros:
+        if i % 2 == 0:
+            lista_pares.append(i)
+        else:
+            lista_impares.append(i)
+    lista_impares.sort()
+    lista_pares.sort()
+    return [lista_impares,lista_pares]
+
+# Inicio funcion para convertir los datos a numeros
+def convertir(dato):
+	while dato.isnumeric() == False: # Mientras que el dato del numero no sea un numero
+		print("¡Lo ingresado no es un número!")
+		dato = input("Ingrese el dato nuevamente: ") # Se pide nuevamente el dato
+	dato = int(dato) # Se convierte el dato 
+	return dato
+
+'''while True:
+    print("Desea ingresar un numero:\n 1.Si\n 2.No")
+    opcion = int(input())
+    if opcion == 1:
+        numero_a_ingresar = input("Ingrese el numero: ")
+        numero_a_ingresar = convertir(numero_a_ingresar)
+    elif opcion == 2:
+        separar(lista_ingresada)
+        print(lista_ingresada)
+        print("La lista impar es: {}\nLa lista par es: {}".format(lista_ingresada[0],lista_ingresada[1]))
+        break
+    else:
+        print("Ingreso opcion incorrecta")'''
+
+
+
+
+
